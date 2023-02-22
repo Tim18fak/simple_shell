73ef1ae922d4da55b0f01136fc0849bd37a78ef3
boatengjulius14 <boatengjulius14@gmail.com>
2023-02-22 08:45:43 +0000

    builtins

--- builtins_list.c
+++ builtins_list.c
@@ -5,7 +5,7 @@
  * @data: struct for the program's data
  * Return: Returns the return of the function executed is there is a match,
  * otherwise returns -1.
- **/
+ */
 int builtins_list(data_of_program *data)
 {
 	int iterator;
@@ -20,16 +20,12 @@ int builtins_list(data_of_program *data)
 		{NULL, NULL}
 	};
 
-/*walk through the structure*/
 	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
 	{
-/*if there is a match between the given command and a builtin,*/
 		if (str_compare(options[iterator].builtin, data->command_name, 0))
 		{
-/*execute the function, and return the return value of the function*/
 			return (options[iterator].function(data));
 		}
-/*if there is no match return -1 */
 	}
 	return (-1);
 }
